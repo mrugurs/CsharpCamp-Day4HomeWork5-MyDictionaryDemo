202f1f9934d648f48fafaecada9608fbf1b44091
Murat UGUR <mr.ugurs@hotmail.com>
2021-02-01 00:03:48 +0300

    Add project files.

---
+++ MyDictionaryDemo/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<K, V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKey = keys; //geçici liste
+            V[] tempValue = values; //geçici liste
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public void Write()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " ==> " + values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> enTr = new MyDictionary<string, string>();
+            enTr.Add("Pencil", "Kalem");
+            enTr.Add("Book", "Kitap");
+            enTr.Add("Plate", "Tabak");
+            enTr.Add("Table", "Masa");
+            enTr.Add("School", "Okul");
+            enTr.Add("Car", "Araba");
+
+            enTr.Write();
+        }
+    }
+}
